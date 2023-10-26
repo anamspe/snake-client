1265034528b7d88cf30ccb55ee135f39bd20d47e
Ana <anmeuri@gmail.com>
2023-10-25 22:57:42 -0700

    Created constants.js to include some important variables that won't change in the future

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require('net');
+const { IP, PORT, NAME, CONNECTED } = require('./constants')
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
@@ -12,13 +13,13 @@ const connect = function() {
   
   //function to alert that connection has been successfully established
   conn.on("connect", () => {
-    console.log('Successfully connected to game server');
+    console.log(CONNECTED);
   });
 
   //function to send player name when it connects to server
   //function to send a "Move: up" command as soon as it connects
   conn.on("connect", () => {
-    conn.write('Name: AMF');
+    conn.write(NAME);
     // conn.write('Move: up');
   });
 
---
+++ constants.js
@@ -0,0 +1,12 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+
+const CONNECTED = 'Successfully connected to game server';
+const NAME = 'Name: AMF';
+
+module.exports = {
+  IP,
+  PORT,
+  CONNECTED,
+  NAME,
+};
