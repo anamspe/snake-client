3a5695ed5fca897fdd389278c4b20bdc3d6f34c7
Ana <anmeuri@gmail.com>
2023-10-25 20:16:01 -0700

    No significant changes

--- client.js
+++ client.js
@@ -16,9 +16,12 @@ const connect = function() {
   });
 
   //function to send player name when it connects to server
+  //function to send a "Move: up" command as soon as it connects
   conn.on("connect", () => {
     conn.write('Name: AMF');
+    // conn.write('Move: up');
   });
+
 
   // function to handle incoming data and console.log it for the player
   conn.on("data", (data) => {
