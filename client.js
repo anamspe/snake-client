f7089e9dd150035c86638bbf65c16fdc7e2d0845
Ana <anmeuri@gmail.com>
2023-10-25 20:00:21 -0700

    Implemented printing messages for when the connection is successful and name command

--- client.js
+++ client.js
@@ -9,11 +9,21 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  
+  //function to alert that connection has been successfully established
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+  });
+
+  //function to send player name when it connects to server
+  conn.on("connect", () => {
+    conn.write('Name: AMF');
+  });
 
   // function to handle incoming data and console.log it for the player
   conn.on("data", (data) => {
     console.log('player says:', data);
-  });
+  });  
 
   return conn;
 };
